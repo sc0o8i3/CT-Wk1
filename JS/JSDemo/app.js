23e403cd678b9af9efeb9699f30d8267de9bbdf4
Peter Pastor <peterpastor02@gmail.com>
2026-03-01 19:10:44 -0800

    changing directory tree again to reflect the different modules and the assignments under each

---
+++ JS/JSDemo/app.js
@@ -0,0 +1,35 @@
+console.log("Hello World!");
+console.log("This is a test of the console log.");
+
+console.log("Today’s weather is sunny");
+console.log(10 * 3);
+console.log("Logged in: " + true);
+
+let name = "Ali";
+let age = 22;
+const country = "Egypt";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Country:", country);
+
+let x = 10;
+let y = 5;
+let total = x + y;
+
+console.log("Total:", total); // 15
+console.log("Is x greater than y?", x > y); // true
+
+let isLoggedIn = true;
+let hasPermission = false;
+
+console.log("isLoggedIn && hasPermission:", isLoggedIn && hasPermission); // false
+console.log("isLoggedIn || hasPermission:", isLoggedIn || hasPermission); // true
+
+let groceries = ["bread", "milk", "eggs"];
+
+console.log("First item:", groceries[0]);
+groceries.push("cheese");
+console.log("Updated list:", groceries);
+groceries[1] = "almond milk";
+console.log("Modified list:", groceries);
